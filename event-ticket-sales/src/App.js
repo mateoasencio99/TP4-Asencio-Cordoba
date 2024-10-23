6518c07cd071069d240dfc4d33cae98dcb549b01
Mateo Asencio <mateoasencio99@gmail.com>
2024-10-22 21:42:17 -0300

    Agrego bootrap

--- event-ticket-sales/src/App.js
+++ event-ticket-sales/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Nav from "./nav/nav";
+
 
 function App() {
   return (
