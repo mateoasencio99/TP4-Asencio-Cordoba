7cc6540a8db1cccfd958cafe37dbb85a2dedd576
Lucas-Cordoba <cssmcordoba@gmail.com>
2024-10-29 19:56:17 -0300

    'estilo principal'

--- event-ticket-sales/src/App.js
+++ event-ticket-sales/src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Nav from "./nav/nav";
 import Principal from "./principal/principal";
-import Footer from "./footer/footer"
-
+import Footer from "./footer/footer";
+// import Eventos from "./evento/evento";
+// import Detalles from "./detalles/detalles"; 
+// import Comprar from "./comprar/comprar"; 
+// import Login from "./login/login";
+// import Register from "./register/register"; 
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Principal/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Principal />} />
+          {/* <Route path="/eventos" element={<Eventos />} /> */}
+          {/* <Route path="/detalles/:id" element={<Detalles />} />
+          <Route path="/comprar/:id" element={<Comprar />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Register />} /> */}
+          
+          {/* Ruta para redirigir a la página principal si no se encuentra la ruta */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- event-ticket-sales/src/principal/principal.js
+++ event-ticket-sales/src/principal/principal.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Carousel from "../carousel/carousel";
-import './principal.css';
+import "./principal.css";
 import { Link } from "react-router-dom";
 
 const Principal = () => {
@@ -9,64 +9,75 @@ const Principal = () => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch('http://localhost:8080/api/upcomingEvents'); 
+        const response = await fetch(
+          "http://localhost:8080/api/upcomingEvents"
+        );
         if (!response.ok) {
-          throw new Error('Error al obtener los eventos');
+          throw new Error("Error al obtener los eventos");
         }
         const data = await response.json();
-        setEvents(data); 
+        setEvents(data);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
 
     fetchEvents();
-  }, []); 
+  }, []);
 
   const formatFecha = (fecha) => {
-      const opciones = { day: '2-digit', month: '2-digit', year: 'numeric' };
-      return new Date(fecha).toLocaleDateString('es-ES', opciones).replace(/\//g, '/'); // Cambia las barras si es necesario
+    const opciones = { day: "2-digit", month: "2-digit", year: "numeric" };
+    return new Date(fecha)
+      .toLocaleDateString("es-ES", opciones)
+      .replace(/\//g, "/"); // Cambia las barras si es necesario
   };
 
   return (
     <div>
       <div className="container">
-        <h2 className="text-center my-5">ANUNCIOS</h2>
+        <h2 className="text-center my-5 fw-3 fs-2">ANUNCIOS</h2>
         <Carousel />
       </div>
       <hr></hr>
       <div className="container">
-        <h2 className="text-center mb-5">PRÓXIMOS EVENTOS</h2>
+        <h2 className="text-center mb-5 fw-3 fs-2">PRÓXIMOS EVENTOS</h2>
         <div className="row">
-        {events.map(event => (
-          <div className="col-md-4 mb-4">
-            <div className="card h-100 shadow">
+          {events.map((event) => (
+            <div className="col-md-4 mb-4">
+              <div className="card h-100 shadow bc">
                 <img
-                    src={require(`../img/events/${event.id}.jpg`)}
-                    className="card-img-top event-image"
-                    alt={`Imagen de ${event.name}`}
+                  src={require(`../img/events/${event.id}.jpg`)}
+                  className="card-img-top event-image"
+                  alt={`Imagen de ${event.name}`}
                 />
                 <div className="card-body text-center">
-                    <h5 className="card-title">{event.name}</h5>
-                    <p className="card-text">${event.price}</p>
-                    <p className="card-text">{formatFecha(event.date)}</p>
-                    {/* <Link to={`/detalles/${event.id}`} className="btn btn-primary">
-                        Ver detalles
-                    </Link> */}
+                  <h5 className="card-title fs-4 fw-bold text-dark">
+                    {event.name}
+                  </h5>
+                  <p className="card-text fs-5 text-success">${event.price}</p>
+                  <p className="card-text text-muted">
+                    {formatFecha(event.date)}
+                  </p>
+                  <Link
+                    to={`/detalles/${event.id}`}
+                    className="btn btn-dark fs-6 fw-bold boton"
+                  >
+                    Ver detalles
+                  </Link>
                 </div>
+              </div>
             </div>
+          ))}
         </div>
-        ))}
-        </div>
-      </div> 
+      </div>
       <hr></hr>
       <div className="container mb-80">
-        <h2 className="text-center mb-5">SOBRE NOSOTROS</h2>
+        <h2 className="text-center mb-5 fw-3 fs-2">SOBRE NOSOTROS</h2>
         <div className="row">
           <div className="col-md-6">
             <img
               src={require("../img/2.jpg")}
-              className="d-block w-100"
+              className="d-block w-100 bs ml-1"
               alt="Imagen Organizador"
             />
           </div>
